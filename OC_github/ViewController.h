8c98cd90a130daefadb45daeb8b0156d56560f9e
iOSFQ <847889612@qq.com>
2016-05-31 11:14:47 +0800

    init

---
+++ OC_github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_github
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 拾个月亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC_github/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  OC_github
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 拾个月亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //这是初始化创建
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
